0933c503bbea353477f1b63195c57470a0ca1a50
Mañana <Maana@AULA8-24>
2024-04-16 10:00:34 +0200

    Ejercicio 2

---
+++ ev3_entornos_achraf_hadi/src/ejercicio_2/Pedido.java
@@ -0,0 +1,12 @@
+package ejercicio_2;
+
+public class Pedido {
+	
+	private int numPedido;
+	
+	public double calcularPrecioTotalPedido() {
+
+		// Lógica para calcular el precio total de un pedido
+
+	}
+}
---
+++ ev3_entornos_achraf_hadi/src/ejercicio_2/Tienda.java
@@ -0,0 +1,17 @@
+package ejercicio_2;
+
+import java.util.List;
+
+public class Tienda {
+
+	private List<Producto> productos;
+	private List<Cliente> clientes;
+	private List<Pedido> pedidos;
+
+	public void realizarPedido(Cliente cliente, List<Producto> productos) {
+
+		// Lógica para realizar un pedido
+
+	}
+// Otros métodos relacionados con la gestión de la tienda
+}
